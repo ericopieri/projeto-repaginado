5de56bf7577162028b0cf4188f646641e03cc383
Erico Pieri <erico.pieri@gmail.com>
2022-08-15 19:27:44 -0300

    término parte básica do header

--- src/layouts/EstruturaPadrao.vue
+++ src/layouts/EstruturaPadrao.vue
@@ -1,5 +1,30 @@
 <template>
-  <div id="estrutura-padrao"></div>
+  <div id="estrutura-padrao">
+    <header class="header">
+      <div class="header-est centralizado">
+        <img
+          src="../assets/logos/logo_header.png"
+          alt="Logo DeepShoes"
+          id="logo-header"
+        />
+        <nav id="nav-header">
+          <ul>
+            <li>
+              <a href="#"
+                ><font-awesome-icon icon="fa-solid fa-magnifying-glass"
+              /></a>
+            </li>
+            <li>
+              <a href="#"
+                ><font-awesome-icon icon="fa-solid fa-cart-shopping"
+              /></a>
+            </li>
+            <li><a href="#">Entrar</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
